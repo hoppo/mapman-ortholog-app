c34d43b00ba9db1b8bb09ae0727e80932418572d
marie <marie@bangor.plabipd.de>
2018-01-09 14:36:16 +0100

    some fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,118 +1,121 @@
-/* globals window, d3, $ */
-'use strict';
-window.addEventListener('Agave::ready', function() {
+/*global _*/
+/*jshint camelcase: false*/
+(function(window, $, _, undefined) {
+	'use strict';
 
 	var appContext = $('[data-app-name="mapman-ortholog-finder"]');
-
-	var form = $('form[name=ortholog_finder_form]', appContext);
-
-	form.on('submit', function(e) {
-		e.preventDefault();
+	
+	
+	window.addEventListener('Agave::ready', function() {
 
 		var Agave = window.Agave;
-		
-		$('#mapman_ortholog_finder-table', appContext).empty();
-		$('#mapman_ortholog_finder-messages', appContext).empty();
-		$('.has-error', appContext).removeClass('has-error');
-
-		var hasError = false;
-
-		
-		var locusString = $.trim(this.geneLocus.value);
-		console.log(locusString);
-				
-		if (!locusString){
-			hasError = true;
-			$(this.geneLocus).parent().addClass('has-error');
-			$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Please enter a AGI code</div>');
-		}else if (locusString.split(/\s+/) > 1){
-			$(this.geneLocus).parent().addClass('has-error');
-			$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Only one gene can be entered</div>');
-			hasError = true; 
-		}
-
-		
-		if (!hasError){
-			
-			var queryParams ={'aragene':locusString};
-		
-			Agave.api.adama.getStatus({}, function(resp) {
-				if (resp.obj.status === 'success') {
-					$.ajax({   
-						url: 'https://api.araport.org/community/v0.3/mercator/orthologfinder_v0.1/access',
-						headers: {'Authorization': 'Bearer ' + Agave.token.accessToken},
-						data: queryParams,
-						error: function(err) {
-							console.log(err);
-							$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Unexpected Error occured</div>');
-						},
-						success: function(data) {
-							console.log(data);
-							showResults(data);
-						}
-					});
 	
-				}else {
-					$('mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">The Query Service is currently unavailable. Please try again later.</div>');
-				}
-			});
-		}
+		var form = $('form[name=ortholog_finder_form]', appContext);
+
+		form.on('submit', function(e) {
+			e.preventDefault();
+
 		
-		function showResults(data){
-			
-			if(data.status === 'success'){
+			$('#mapman_ortholog_finder-table', appContext).empty();
+			$('#mapman_ortholog_finder-messages', appContext).empty();
+			$('.has-error', appContext).removeClass('has-error');
+
+			var hasError = false;
+
+		
+			var locusString = $.trim(this.geneLocus.value);
 				
-				if(data.result.hasOwnProperty('35.2')){
-					$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-info">Genes of Interest is assigned to the Unknown bin</div>');
-				}else{
-					var mangleData=mangleJson(data);
-					tabulate(mangleData);
-					//$('#mapman_ortholog_finder-table .table').dataTable();
-				}
-			}else{
-				$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Genes of Interest could not be found</div>');
+			if (!locusString){
+				hasError = true;
+				$(this.geneLocus).parent().addClass('has-error');
+				$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Please enter a AGI code</div>');
+			}else if (locusString.split(/\s+/) > 1){
+				$(this.geneLocus).parent().addClass('has-error');
+				$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Only one gene can be entered</div>');
+				hasError = true; 
 			}
-		}
+	
+			
+			if (!hasError){
+				
+				var queryParams ={'aragene':locusString};
+			
+				Agave.api.adama.getStatus({}, function(resp) {
+					if (resp.obj.status === 'success') {
+						$.ajax({   
+							url: 'https://api.araport.org/community/v0.3/mercator/orthologfinder_v0.1/access',
+							headers: {'Authorization': 'Bearer ' + Agave.token.accessToken},
+							data: queryParams,
+							error: function(err) {
+								console.log(err);
+								$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Unexpected Error occured</div>');
+							},
+							success: function(data) {
+								console.log(data);
+								showResults(data);
+							}
+						});
 		
-		function mangleJson(data) {
-
-			var result=data.result;
-			var out=[];
-
-			for (var i in result){
-
-				if (i !== 'query'){
-					var binArray=[];
-					binArray.push(i);
-					out.push(binArray);
-
-					var binResult=result[i];
+					}else {
+						$('mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">The Query Service is currently unavailable. Please try again later.</div>');
+					}
+				});
+			}
+			
+			function showResults(data){
+				
+				if(data.status === 'success'){
 					
-					for(var species in binResult){
-						var genes = binResult[species];
-						genes.splice(0, 0, species);
-						out.push(genes);
+					if(data.result.hasOwnProperty('35.2')){
+						$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-info">Genes of Interest is assigned to the Unknown bin</div>');
+					}else{
+						var mangleData=mangleJson(data);
+						tabulate(mangleData);
+					}
+				}else{
+					$('#mapman_ortholog_finder-messages', this).append('<div class="alert alert-danger">Genes of Interest could not be found</div>');
+				}
+			}
+			
+			function mangleJson(data) {
+	
+				var result=data.result;
+				var out=[];
+	
+				for (var i in result){
+	
+					if (i !== 'query'){
+						var binArray=[];
+						binArray.push(i);
+						out.push(binArray);
+	
+						var binResult=result[i];
+						
+						for(var species in binResult){
+							var genes = binResult[species];
+							genes.splice(0, 0, species);
+							out.push(genes);
+						}
 					}
 				}
+				return out;
 			}
-			return out;
-		}
-		 
-		function tabulate(data) {
-			var table = d3.select('#mapman_ortholog_finder-table').append('table').attr('class', 'table');
-			table.append('thead');
-			var tbody = table.append('tbody');
-
-
-			var rows = tbody.selectAll('tr')
-							.data(data)
-							.enter()
-							.append('tr');
-
-			rows.selectAll('td').data( function(d) { return d; })
-								.enter().append('td')
-								.text(function(d) { return d; });
-		}
-
+			 
+			function tabulate(data) {
+				var table = d3.select('#mapman_ortholog_finder-table').append('table').attr('class', 'table');
+				table.append('thead');
+				var tbody = table.append('tbody');
+	
+	
+				var rows = tbody.selectAll('tr')
+								.data(data)
+								.enter()
+								.append('tr');
+	
+				rows.selectAll('td').data( function(d) { return d; })
+									.enter().append('td')
+									.text(function(d) { return d; });
+			}
+		});
 	});
-});
+})(window, jQuery, _);
